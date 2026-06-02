c07568b0cd94bd7f156561d0442a70953f3e0b67
viswa <viswam.ekula@gmail.com>
2026-06-02 12:21:39 +0530

    changes in conftest.py file

--- conftest.py
+++ conftest.py
@@ -3,6 +3,12 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.edge.options import Options as EdgeOptions
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.edge.service import Service as EdgeService
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 from utilities.config import Config
 
@@ -35,12 +41,17 @@ def setup_teardown(request):
     if request.config.getoption("--headless"):
         options.add_argument("--headless")
 
+        # Required for GitHub Actions/Linux
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--window-size=1920,1080")
+
     if browser == "chrome":
-        request.cls.driver = webdriver.Chrome(options=options)
+        request.cls.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),options=options)
     elif browser == "edge":
-        request.cls.driver = webdriver.Edge(options=options)
+        request.cls.driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()),options=options)
     elif browser == "firefox":
-        request.cls.driver = webdriver.Firefox(options=options)
+        request.cls.driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()),options=options)
 
     request.cls.driver.maximize_window()
 
