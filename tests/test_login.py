fb0e52bae7735540d7ac0e431ebd47efea1eb9f1
viswa <viswam.ekula@gmail.com>
2026-06-02 14:45:07 +0530

    added parametrize for the login testcase

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,15 +1,18 @@
+import pytest
 from page_objects.login_page import LoginPage
+from test_data.login_data import login_data
 from utilities.config import Config
 from utilities.logger import get_logger
 
 log = get_logger()
 class TestLogin:
 
-    def test_login(self):
+    @pytest.mark.parametrize("username, password",login_data)
+    def test_login(self, username, password):
         self.driver.get(Config.get_url())
 
         login_page = LoginPage(self.driver)
 
-        login_page.login(Config.get_username(),
-                         Config.get_password())
+        login_page.login(username, password)
+        log.info(login_page.get_title())
         log.info("logged in")
